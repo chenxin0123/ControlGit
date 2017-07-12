fccd9e07150cb5d1680f229b4b0380c455b583e9
chenxin <407008175@qq.com>
2017-07-12 10:59:44 +0800

    ..

--- dragon_archive_ipa/archiveIpa_ty.sh
+++ dragon_archive_ipa/archiveIpa_ty.sh
@@ -9,9 +9,9 @@ fi
 ./buildLibcosRelease.sh
 ./DistrbuteIOSTiyan.sh
 
-#./uploadIpa_ty_fir.sh
+./uploadIpa_ty_fir.sh &
+./uploadIpa_ty_itc.sh
 #osascript -e 'tell application "Terminal" to activate' -e 'tell application "System Events" to tell process "Terminal" to keystroke "t" using command down' -e 'tell application "Terminal" to do script "sh uploadIpa_ty_fir.sh" in selected tab of the front window'
-#./uploadIpa_ty_itc.sh
 #osascript -e 'tell application "Terminal" to activate' -e 'tell application "System Events" to tell process "Terminal" to keystroke "t" using command down' -e 'tell application "Terminal" to do script "sh uploadIpa_ty_itc.sh" in selected tab of the front window'
-
+wait
 python SendEmail.py "Finish upload costy $NEW_VERSION(build$NEW_BUILD) to appStore and fir" ./archive_ty_upload_fir/fir-cos_tiyan.png
