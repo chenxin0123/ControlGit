f07acba0a381443178e01c05d0761799aaf65a2a
chenxin <407008175@qq.com>
2017-07-13 17:09:49 +0800

    update

--- KillPid.sh
+++ KillPid.sh
@@ -7,7 +7,6 @@ pid1=$(/usr/libexec/PlistBuddy -c "print pid1" ${CONFIG_PATH})
 pid2=$(/usr/libexec/PlistBuddy -c "print pid2" ${CONFIG_PATH})
 pid3=$(/usr/libexec/PlistBuddy -c "print pid3" ${CONFIG_PATH})
 pid4=$(/usr/libexec/PlistBuddy -c "print pid4" ${CONFIG_PATH})
-echo "OLD_VERSION = $OLD_VERSION OLD_BUILD = $OLD_BUILD NEW_VERSION = $NEW_VERSION NEW_BUILD = $NEW_BUILD"
 
 # 2. Kill
 
