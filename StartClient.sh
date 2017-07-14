2f20f3c0ba5607386beac0f1f5d86832cb629090
chenxin <407008175@qq.com>
2017-07-14 11:55:52 +0800

    update

--- StartClient.sh
+++ StartClient.sh
@@ -1,5 +1,12 @@
-./CWatchArchive.sh &
-./CWatchKill.sh &
-./CWatchPrintProcess.sh &
-./CWatchPull.sh &
-./CWatchPush.sh &
+#./CWatchArchive.sh &
+#./CWatchKill.sh &
+#./CWatchPrintProcess.sh &
+#./CWatchPull.sh &
+#./CWatchPush.sh &
+
+
+osascript -e 'tell application "Terminal" to activate' -e 'tell application "System Events" to tell process "Terminal" to keystroke "t" using command down' -e 'tell application "Terminal" to do script "sh CWatchArchive.sh" in selected tab of the front window'
+osascript -e 'tell application "Terminal" to activate' -e 'tell application "System Events" to tell process "Terminal" to keystroke "t" using command down' -e 'tell application "Terminal" to do script "sh CWatchKill.sh" in selected tab of the front window'
+osascript -e 'tell application "Terminal" to activate' -e 'tell application "System Events" to tell process "Terminal" to keystroke "t" using command down' -e 'tell application "Terminal" to do script "sh CWatchPrintProcess.sh" in selected tab of the front window'
+osascript -e 'tell application "Terminal" to activate' -e 'tell application "System Events" to tell process "Terminal" to keystroke "t" using command down' -e 'tell application "Terminal" to do script "sh CWatchPull.sh" in selected tab of the front window'
+osascript -e 'tell application "Terminal" to activate' -e 'tell application "System Events" to tell process "Terminal" to keystroke "t" using command down' -e 'tell application "Terminal" to do script "sh CWatchPush.sh" in selected tab of the front window'
