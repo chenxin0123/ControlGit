6e5bfe6b6ee319cf0865a6f92baf920ed59b5ec6
chenxin <407008175@qq.com>
2017-07-14 10:35:11 +0800

    update

--- PrintProcess.sh
+++ PrintProcess.sh
@@ -1,14 +1,9 @@
 ORIGIN_PATH=$PWD
 CONFIG_PATH="$ORIGIN_PATH/killpid.plist"
 
-# 1. Read config
 logpid=$(/usr/libexec/PlistBuddy -c "print logpid" ${CONFIG_PATH})
-
-# 2. Kill
-
 if [ $logpid -gt 0 ];then
-kill $pid1
-/usr/libexec/PlistBuddy -c "Set:pid1 0" ${CONFIG_PATH}
+/usr/libexec/PlistBuddy -c "Set:logpid 0" ${CONFIG_PATH}
 fi
 ps aux | grep -E "Archive|archive|uploadIpa|watch|Watch" > log/pid.txt
 cat log/pid.txt
