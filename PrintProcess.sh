3d1c6e33c19251955029bc17ed66e0dc49e47de6
chenxin <407008175@qq.com>
2017-07-14 10:36:08 +0800

    update

--- PrintProcess.sh
+++ PrintProcess.sh
@@ -4,6 +4,6 @@ CONFIG_PATH="$ORIGIN_PATH/killpid.plist"
 logpid=$(/usr/libexec/PlistBuddy -c "print logpid" ${CONFIG_PATH})
 if [ $logpid -gt 0 ];then
 /usr/libexec/PlistBuddy -c "Set:logpid 0" ${CONFIG_PATH}
-fi
 ps aux | grep -E "Archive|archive|uploadIpa|watch|Watch" > log/pid.txt
-cat log/pid.txt
+fi
+ps aux | grep -E "Archive|archive|uploadIpa|watch|Watch"
