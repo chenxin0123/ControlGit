07edd7ea4b84bc0c8b0fafd4f4db4ccd75cba87b
chenxin <407008175@qq.com>
2017-07-14 10:32:13 +0800

    update

--- KillPid.sh
+++ KillPid.sh
@@ -7,6 +7,8 @@ pid1=$(/usr/libexec/PlistBuddy -c "print pid1" ${CONFIG_PATH})
 pid2=$(/usr/libexec/PlistBuddy -c "print pid2" ${CONFIG_PATH})
 pid3=$(/usr/libexec/PlistBuddy -c "print pid3" ${CONFIG_PATH})
 pid4=$(/usr/libexec/PlistBuddy -c "print pid4" ${CONFIG_PATH})
+pid5=$(/usr/libexec/PlistBuddy -c "print pid5" ${CONFIG_PATH})
+pid6=$(/usr/libexec/PlistBuddy -c "print pid6" ${CONFIG_PATH})
 
 # 2. Kill
 
@@ -23,6 +25,16 @@ fi
 if [ $pid3 -gt 0 ];then
 kill $pid3
 /usr/libexec/PlistBuddy -c "Set:pid3 0" ${CONFIG_PATH}
+fi
+
+if [ $pid4 -gt 0 ];then
+kill $pid4
+/usr/libexec/PlistBuddy -c "Set:pid4 0" ${CONFIG_PATH}
+fi
+
+if [ $pid4 -gt 0 ];then
+kill $pid4
+/usr/libexec/PlistBuddy -c "Set:pid4 0" ${CONFIG_PATH}
 fi
 
 if [ $pid4 -gt 0 ];then
